6c4d694932753d1dfdefb42d979f7ea095c5b685
Daniel Mendez <awilddanoru@gmail.com>
2022-05-17 16:02:41 -0700

    Initial commit.

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header"
+import Footer from "./Footer"
+
+function App() {
+  return (
+    <div>
+      <Header />
+        <p>Tablekeeper is a web application currently in development to manage your board game nights with efficiency and fun!</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
