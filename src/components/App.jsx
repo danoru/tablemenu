60735e489e7a6a152fd8517072dd523fc6d10e23
Daniel Mendez <awilddanoru@gmail.com>
2022-05-18 12:18:02 -0700

    Minor design changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,24 @@
 import React from "react";
-import Header from "./Header"
-import Footer from "./Footer"
+import Header from "./Header";
+import Footer from "./Footer";
+import Button from "@mui/material/Button"
 
 function App() {
+  fetch()
+
   return (
     <div>
       <Header />
         <p>Tablekeeper is a web application currently in development to manage your board game nights with efficiency and fun!</p>
+        <div className="search">
+          <input type="search" placeholder="Board Game" />
+          <Button>Add</Button>
+        </div>
+        <div>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
       <Footer />
     </div>
   );
