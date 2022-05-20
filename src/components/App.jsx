2b6274dbceafdd405eeaf9595c69fd2e8b3788ae
Daniel Mendez <awilddanoru@gmail.com>
2022-05-20 12:34:11 -0700

    Added functionality for making inventory list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
+import InventoryList from "./InventoryList"
 import InputSearch from "./InputSearch"
 import Header from "./Header";
 import Footer from "./Footer";
 
 
 function App() {
-    
+  const [inventory, setInventory] = useState([]);
+
+  function addGame(inputText) {
+    setInventory((prevInventory) => {
+      return [...prevInventory, inputText];
+    });
+  }
+
+  function removeGame(id) {
+    setInventory((prevInventory) => {
+      return prevInventory.filter((inventory, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <p>Tablekeeper is a web application currently in development to manage your board game nights with efficiency and fun!</p>
-      <InputSearch />
-      {/* <div>
+      <p>Tablekeeper is a web application currently in development to manage your board game nights with efficiency and a bit of flair!</p>
+      <InputSearch onAdd={addGame}/>
+      <div>
         <ul>
+          {inventory.map((gameItem, index) => (
+            <InventoryList
+              key={index}
+              id={index}
+              text={gameItem}
+              onChecked={removeGame}
+            />
+          ))}
           <li></li>
         </ul>
-      </div> */}
+      </div>
       <Footer />
     </div>
   );
--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
-import axios from "axios";
+// import axios from "axios";
 
-function InputSearch() {
+function InputSearch(props) {
   const [inputText, setInputText] = useState("");
 
   function handleChange(event) {
@@ -9,19 +9,27 @@ function InputSearch() {
     setInputText(newValue);
   }
 
-  function handleClick(event) {
-    const newValue = event.target.value;
-    setInputText(newValue.replace(" ","+"));
-    axios.get("https://www.boardgamegeek.com/xmlapi2/search?query="+inputText)
-      .then((response) => {console.log(response.data)})
-  }
+  // Note to Self: This handleClick is for the eventual inclusion of the BGG API. Need to figure out how to turn XML into JSON to grab values from the API before this can work as intended.
+
+  // function handleClick(event) {
+  //   const newValue = event.target.value
+  //   setInputText(newValue.replace(" ","+"));
+  //   axios.get("https://www.boardgamegeek.com/xmlapi2/search?query="+inputText)
+  //     .then((response) => {console.log(response.data)})
+  // }
 
   return (
     <div className="search">
-      <input onChange={handleChange} placeholder="Search Board Games" value={inputText}/>
-      <button onClick={handleClick}>Add</button>
-    </div>
-        
+      <input onChange={handleChange} type="text" placeholder="Search Board Games" input={inputText}/>
+      <button
+        onClick={()=> {
+          props.onAdd(inputText);
+          setInputText("");
+        }}
+      >
+        <span>Add</span>
+      </button>
+    </div> 
   )
 }
 
