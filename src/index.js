cbc5a2e3d66f63075c2a128c4db8f963fd60f221
Daniel Mendez <awilddanoru@gmail.com>
2022-07-28 12:54:21 -0700

    Updated root render to adhere to React 18 requirements

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
@@ -21,4 +21,6 @@ export default function App() {
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root")
+const root = ReactDOM.createRoot(container)
+root.render(<App/>)
