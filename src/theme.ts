fb7f124d418076f6d8b6bfe2c436d8cbfa254543
Daniel Mendez <mdmendomo@gmail.com>
2025-11-11 09:51:56 -0800

    refactor: Update theme

--- src/theme.ts
+++ src/theme.ts
@@ -12,10 +12,10 @@ const theme = createTheme({
   cssVariables: true,
   palette: {
     primary: {
-      main: "#556cd6",
+      main: "#343A40",
     },
     secondary: {
-      main: "#19857b",
+      main: "#ffc107",
     },
     error: {
       main: red.A400,
